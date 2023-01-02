739946847a16855712773b4c2eafa6e6f6ac365a
Ratul sharker <sharker.ratul.08@gmail.com>
2023-01-02 19:56:42 +0600

    - csvtojson dependency added. - sample conversion verified.

--- src/index.js
+++ src/index.js
@@ -1,4 +1,18 @@
 require('dotenv').config();
+const csv = require("csvtojson");
 
 
-console.log(process.env);
+// console.log(process.env);
+const csvFilePath='Jira Sprint 18.csv';
+
+csv()
+.fromFile(csvFilePath)
+.then((jsonObj)=>{
+    console.log(jsonObj);
+    /**
+     * [
+     * 	{a:"1", b:"2", c:"3"},
+     * 	{a:"4", b:"5". c:"6"}
+     * ]
+     */ 
+});
