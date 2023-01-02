1776deafbca583479b5700a9030844436287e327
Ratul Sharker <sharker.ratul.08@gmail.com>
2023-01-03 03:35:42 +0600

    - all filters moved to a single place. - updated at filter in between added.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 const csv = require("csvtojson");
+const moment = require("moment");
 
 
 async function readCSVAsJson(inputCsvFilePath) {
@@ -7,22 +8,28 @@ async function readCSVAsJson(inputCsvFilePath) {
 }
 
 const csvRowFilters = {
-	assigneeIsNotEmpty : (assigneeFieldName) => {
+	assigneeIsNotEmpty: (assigneeFieldName) => {
 		return (row) => {
 			const assignee = row[assigneeFieldName];
 			return assignee != "";
 		};
 	},
-	equalSprintName : (sprintFieldName, sprintName) => {
+	equalSprintName: (sprintFieldName, sprintName) => {
 		return (row) => {
 			const sprint = row[sprintFieldName];
 			return sprint == sprintName;
 		};
 	},
-	testedByNotEmpty : (testedByFieldName) => {
+	testedByNotEmpty: (testedByFieldName) => {
 		return (row) => {
 			const testedBy = row[testedByFieldName];
 			return testedBy != "";
+		};
+	},
+	testedByEmpty: (testedByFieldName) => {
+		return (row) => {
+			const testedBy = row[testedByFieldName];
+			return testedBy == "";
 		};
 	},
 	statusEqual: (statusFieldName, statusName) => {
@@ -36,105 +43,137 @@ const csvRowFilters = {
 			const status = row[statusFieldName];
 			return status != statusName;
 		};
+	},
+	updatedBetween: (updatedFieldName, startMomentDate, endMomentDate) => {
+		return (row) => {
+			const updated = moment(row[updatedFieldName], "DD/MMM/YY");
+			return updated.isSameOrAfter(startMomentDate) && updated.isSameOrBefore(endMomentDate);
+		}
 	}
 }
 
-function prepareReportDataForPerAssigneePerStatusIssueCount(csvRows, sprintName, assigneeFieldName, statusFieldName, sprintFieldName) {
+function prepareReportDataForPerAssigneePerStatusIssueCount(csvRows, sprintName, assigneeFieldName, statusFieldName, sprintFieldName, updatedFieldName, sprintStartMomentDate, sprintEndMomentDate) {
 	const perAssigneePerStatusIssueCount = {};
-	csvRows.filter(csvRowFilters.equalSprintName(sprintFieldName, sprintName))
-	.filter(csvRowFilters.assigneeIsNotEmpty(assigneeFieldName))
-	.forEach(csvRow => {
-		const assignee = csvRow[assigneeFieldName];
-		const status = csvRow[statusFieldName];
+	csvRows
+		.filter(csvRowFilters.updatedBetween(updatedFieldName, sprintStartMomentDate, sprintEndMomentDate))
+		.filter(csvRowFilters.assigneeIsNotEmpty(assigneeFieldName))
+		.forEach(csvRow => {
+			const assignee = csvRow[assigneeFieldName];
+			const status = csvRow[statusFieldName];
 
-		const statusWiseIssueCount = perAssigneePerStatusIssueCount[assignee] || {};
-		const issueCount = statusWiseIssueCount[status] ? statusWiseIssueCount[status] + 1 : 1;
-		
-		statusWiseIssueCount[status] = issueCount;
-		perAssigneePerStatusIssueCount[assignee] = statusWiseIssueCount;
-	});
+			const statusWiseIssueCount = perAssigneePerStatusIssueCount[assignee] || {};
+			const issueCount = statusWiseIssueCount[status] ? statusWiseIssueCount[status] + 1 : 1;
+
+			statusWiseIssueCount[status] = issueCount;
+			perAssigneePerStatusIssueCount[assignee] = statusWiseIssueCount;
+		});
 
 	return perAssigneePerStatusIssueCount;
 }
 
-function prepareReportDataForPerAssigneePerStatusStoryPoints(csvRows, sprintName, assigneeFieldName, statusFieldName, sprintFieldName, storyPointFieldName) {
+function prepareReportDataForPerAssigneePerStatusStoryPoints(csvRows, sprintName, assigneeFieldName, statusFieldName, sprintFieldName, storyPointFieldName, updatedFieldName, sprintStartMomentDate, sprintEndMomentDate) {
 	const perAssigneePerStatusStoryPoints = {};
-	csvRows.filter(csvRowFilters.equalSprintName(sprintFieldName, sprintName))
-	.filter(csvRowFilters.assigneeIsNotEmpty(assigneeFieldName))
-	.forEach(csvRow => {
-		const assignee = csvRow[assigneeFieldName];
-		const status = csvRow[statusFieldName];
-		let storyPoint = parseFloat(csvRow[storyPointFieldName]);
-		storyPoint = isNaN(storyPoint) ? 0 : storyPoint;
+	csvRows
+		.filter(csvRowFilters.updatedBetween(updatedFieldName, sprintStartMomentDate, sprintEndMomentDate))
+		.filter(csvRowFilters.assigneeIsNotEmpty(assigneeFieldName))
+		.forEach(csvRow => {
+			const assignee = csvRow[assigneeFieldName];
+			const status = csvRow[statusFieldName];
+			let storyPoint = parseFloat(csvRow[storyPointFieldName]);
+			storyPoint = isNaN(storyPoint) ? 0 : storyPoint;
 
-		const statusWiseStoryPoints = perAssigneePerStatusStoryPoints[assignee] || {};
-		const storyPoints = statusWiseStoryPoints[status] != undefined ? statusWiseStoryPoints[status] + storyPoint : 0;
+			const statusWiseStoryPoints = perAssigneePerStatusStoryPoints[assignee] || {};
+			const storyPoints = statusWiseStoryPoints[status] != undefined ? statusWiseStoryPoints[status] + storyPoint : 0;
 
-		statusWiseStoryPoints[status] = storyPoints;
-		perAssigneePerStatusStoryPoints[assignee] = statusWiseStoryPoints;
-	});
+			statusWiseStoryPoints[status] = storyPoints;
+			perAssigneePerStatusStoryPoints[assignee] = statusWiseStoryPoints;
+		});
 
 	return perAssigneePerStatusStoryPoints;
 }
 
-function preparePerTestedByIssueNotInReadyToQA(csvRows, testedByFieldName, statusFieldName) {
+function preparePerTestedByIssueNotInReadyToQA(csvRows, testedByFieldName, statusFieldName, updatedFieldName, sprintStartMomentDate, sprintEndMomentDate) {
 	const perTestedByNotInReadyToQAIssueCount = {};
 
-	csvRows.filter(csvRowFilters.testedByNotEmpty(testedByFieldName))
-	.filter(csvRowFilters.statusNotEqual(statusFieldName, "Ready to QA"))
-	.forEach(csvRow => {
-		const testedBy = csvRow[testedByFieldName];
+	csvRows
+		.filter(csvRowFilters.updatedBetween(updatedFieldName, sprintStartMomentDate, sprintEndMomentDate))
+		.filter(csvRowFilters.testedByNotEmpty(testedByFieldName))
+		.filter(csvRowFilters.statusNotEqual(statusFieldName, "Ready To QA"))
+		.forEach(csvRow => {
+			const testedBy = csvRow[testedByFieldName];
 
-		let testedByIssueCount = perTestedByNotInReadyToQAIssueCount[testedBy] || 0;
-		testedByIssueCount += 1;
+			let testedByIssueCount = perTestedByNotInReadyToQAIssueCount[testedBy] || 0;
+			testedByIssueCount += 1;
 
-		perTestedByNotInReadyToQAIssueCount[testedBy] = testedByIssueCount;
-	});
+			perTestedByNotInReadyToQAIssueCount[testedBy] = testedByIssueCount;
+		});
 
 	return perTestedByNotInReadyToQAIssueCount;
 }
 
-function prepareIssueCountAndStoryPointsInGivenStatusName(csvRows, statusFieldName, storyPointFieldName, statusName) {
+function prepareIssueCountAndStoryPointsInGivenStatusName(csvRows, statusFieldName, storyPointFieldName, statusName, updatedFieldName, sprintStartMomentDate, sprintEndMomentDate) {
 	let issueCount = 0;
 	let storyPoints = 0.0;
 
-	csvRows.filter(csvRowFilters.statusEqual(statusFieldName, statusName))
-	.forEach(csvRow => {
-		issueCount++;
+	csvRows
+		.filter(csvRowFilters.updatedBetween(updatedFieldName, sprintStartMomentDate, sprintEndMomentDate))
+		.filter(csvRowFilters.statusEqual(statusFieldName, statusName))
+		.forEach(csvRow => {
+			issueCount++;
 
-		const storyPoint = parseFloat(csvRow[storyPointFieldName]);
-		if(isNaN(storyPoint) == false) {
-			storyPoints += storyPoint;
-		}
-	});
+			const storyPoint = parseFloat(csvRow[storyPointFieldName]);
+			if (isNaN(storyPoint) == false) {
+				storyPoints += storyPoint;
+			}
+		});
 
 	return {
-		"issueCount" : issueCount,
-		"storyPoints" : storyPoints
+		"issueCount": issueCount,
+		"storyPoints": storyPoints
 	};
+}
+
+function prepareIssueCountTestedByNoneAndStatusIsDone(csvRows, statusFieldName, testedByFieldName, updatedFieldName, sprintStartMomentDate, sprintEndMomentDate) {
+	let issueCount = 0;
+
+	csvRows
+		.filter(csvRowFilters.updatedBetween(updatedFieldName, sprintStartMomentDate, sprintEndMomentDate))
+		.filter(csvRowFilters.testedByEmpty(testedByFieldName))
+		.filter(csvRowFilters.statusEqual(statusFieldName, "Done"))
+		.forEach(row => {
+			issueCount++;
+		});
+
+	return issueCount;
 }
 
 async function readCSVAndGenerateReport() {
 
 	const csvRows = await readCSVAsJson(process.env.INPUT_CSV_FILE_NAME);
 
-	const perAssigneePerStatusIssueCount = prepareReportDataForPerAssigneePerStatusIssueCount(csvRows, process.env.CURRENT_SPRINT, process.env.ASSIGNEE_FIELD_NAME, process.env.STATUS_FIELD_NAME, process.env.SPRINT_FIELD_NAME);
+	const sprintStartMomentDate = moment(process.env.SPRINT_START_DATE, "DD/MM/YYYY");
+	const sprintEndMomentDate = moment(process.env.SPRINT_END_DATE, "DD/MM/YYYY");
+
+	const perAssigneePerStatusIssueCount = prepareReportDataForPerAssigneePerStatusIssueCount(csvRows, process.env.CURRENT_SPRINT, process.env.ASSIGNEE_FIELD_NAME, process.env.STATUS_FIELD_NAME, process.env.SPRINT_FIELD_NAME, process.env.UPDATED_FIELD_NAME, sprintStartMomentDate, sprintEndMomentDate);
 	console.log(perAssigneePerStatusIssueCount);
 
-	const perAssigneePerStatusStoryPoints = prepareReportDataForPerAssigneePerStatusStoryPoints(csvRows, process.env.CURRENT_SPRINT, process.env.ASSIGNEE_FIELD_NAME, process.env.STATUS_FIELD_NAME, process.env.SPRINT_FIELD_NAME, process.env.STORY_POINT_FIELD_NAME);
+	const perAssigneePerStatusStoryPoints = prepareReportDataForPerAssigneePerStatusStoryPoints(csvRows, process.env.CURRENT_SPRINT, process.env.ASSIGNEE_FIELD_NAME, process.env.STATUS_FIELD_NAME, process.env.SPRINT_FIELD_NAME, process.env.STORY_POINT_FIELD_NAME, process.env.UPDATED_FIELD_NAME, sprintStartMomentDate, sprintEndMomentDate);
 	console.log(perAssigneePerStatusStoryPoints);
 
-	const perTestedByIssueNotInReadyToQA = preparePerTestedByIssueNotInReadyToQA(csvRows, process.env.TESTED_BY_FIELD_NAME, process.env.STATUS_FIELD_NAME);
+	const perTestedByIssueNotInReadyToQA = preparePerTestedByIssueNotInReadyToQA(csvRows, process.env.TESTED_BY_FIELD_NAME, process.env.STATUS_FIELD_NAME, process.env.UPDATED_FIELD_NAME, sprintStartMomentDate, sprintEndMomentDate);
 	console.log(perTestedByIssueNotInReadyToQA);
 
-	const issueCountAndStoryPointsInReadyToQA = prepareIssueCountAndStoryPointsInGivenStatusName(csvRows, process.env.STATUS_FIELD_NAME, process.env.STORY_POINT_FIELD_NAME, "Ready To QA");
+	const issueCountAndStoryPointsInReadyToQA = prepareIssueCountAndStoryPointsInGivenStatusName(csvRows, process.env.STATUS_FIELD_NAME, process.env.STORY_POINT_FIELD_NAME, "Ready To QA", process.env.UPDATED_FIELD_NAME, sprintStartMomentDate, sprintEndMomentDate);
 	console.log(issueCountAndStoryPointsInReadyToQA);
 
-	const issueCountAndStoryPointsInDone = prepareIssueCountAndStoryPointsInGivenStatusName(csvRows, process.env.STATUS_FIELD_NAME, process.env.STORY_POINT_FIELD_NAME, "Done");
+	const issueCountAndStoryPointsInDone = prepareIssueCountAndStoryPointsInGivenStatusName(csvRows, process.env.STATUS_FIELD_NAME, process.env.STORY_POINT_FIELD_NAME, "Done", process.env.UPDATED_FIELD_NAME, sprintStartMomentDate, sprintEndMomentDate);
 	console.log(issueCountAndStoryPointsInDone);
 
-	const issueCountAndStoryPointsInBacklog = prepareIssueCountAndStoryPointsInGivenStatusName(csvRows, process.env.STATUS_FIELD_NAME, process.env.STORY_POINT_FIELD_NAME, "Backlog");
+	const issueCountAndStoryPointsInBacklog = prepareIssueCountAndStoryPointsInGivenStatusName(csvRows, process.env.STATUS_FIELD_NAME, process.env.STORY_POINT_FIELD_NAME, "Backlog", process.env.UPDATED_FIELD_NAME, sprintStartMomentDate, sprintEndMomentDate);
 	console.log(issueCountAndStoryPointsInBacklog);
+
+	const directlyMadeDoneIssueCount = prepareIssueCountTestedByNoneAndStatusIsDone(csvRows, process.env.STATUS_FIELD_NAME, process.env.TESTED_BY_FIELD_NAME, process.env.UPDATED_FIELD_NAME, sprintStartMomentDate, sprintEndMomentDate);
+	console.log(directlyMadeDoneIssueCount);
 }
 
 readCSVAndGenerateReport();
